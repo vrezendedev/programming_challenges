9363f9f66e432d85ef6c86e3bea9a6a938b61376
vrezendedev <rezendedev@gmail.com>
2021-12-20 18:21:54 -0300

    First commit

---
+++ cpf_challenge/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafiocpf
+{
+    class Program
+    {
+        static bool VerificarCPF(int[] cpf)
+        {
+            bool cpfValido = false;
+            int soma, mult, dv9, dv10;
+
+            soma = 0;
+            mult = 1;
+
+            for (int i = 0; i < 9; i++)
+            {
+                soma = soma + cpf[i] * mult;
+                mult++;
+            }
+
+            dv9 = soma % 11;
+            if (dv9 == 10)
+            {
+                dv9 = 0;
+            }
+
+
+            soma = 0;
+            mult = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                soma = soma + cpf[i] * mult;
+                mult++;
+            }
+
+            dv10 = soma % 11;
+            if (dv10 == 10)
+            {
+                dv10 = 0;
+            }
+
+            if (dv9 == cpf[9] && dv10 == cpf[10])
+            {
+                cpfValido = true;
+            }
+
+            return cpfValido;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] cpfNums = new int[11];
+
+            Console.Write("Digite o CPF: ");
+            char[] cpfChars = (Console.ReadLine().ToCharArray());
+
+            for (int i = 0; i < cpfChars.Length; i++)
+            {
+                cpfNums[i] = int.Parse(cpfChars[i].ToString());
+            }
+
+            if (VerificarCPF(cpfNums) == true)
+            {
+                Console.WriteLine("O CPF é válido.");
+            }
+            else
+            {
+                Console.WriteLine("O CPF não é válido.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ word_form_int_challenge/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafionumextenso
+{
+    class Program
+    {
+        class Valor
+        {
+            private int valor;
+
+            public Valor()
+            {
+                valor = 0;
+            }
+
+            public int AtribuiValor
+            {
+                set
+                {
+                    this.valor = value;
+                }
+            }
+
+            public int RetornaValor
+            {
+                get
+                {
+                    return this.valor;
+                }
+            }
+
+            public string NumExtenso()
+            {
+                string extenso = string.Empty;
+
+                string[] unidades, dezenas, centenas;
+                unidades = new string[] { "zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove" };
+                dezenas = new string[] { "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa" };
+                centenas = new string[] { "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos" };
+
+                int val = RetornaValor;
+
+                if (val >= 0 && val <= 19)
+                {
+
+                    extenso = unidades[val];
+                    return extenso;
+
+                }
+                else if (val >= 20 && val < 100)
+                {
+                    char[] nums = val.ToString().ToCharArray(); //Converte o valor númerico em array de chars
+                    string i = nums[0].ToString(); //Converte o primeiro valor númerico da array de chars p/ string
+                    int j = int.Parse(i); //Converte a string p/ int
+                    extenso = dezenas[j - 2]; //Pega na array de dezenas o valor correspondente da casa decimal
+                    i = nums[1].ToString();
+                    j = int.Parse(i);
+                    if (j > 0)
+                    {
+                        extenso = extenso + " e " + unidades[j];
+                    }
+                    return extenso;
+                }
+                else if (val >= 100 && val <= 999)
+                {
+                    char[] nums = val.ToString().ToCharArray();
+                    string i = nums[0].ToString();
+                    int j = int.Parse(i);
+                    extenso = centenas[j - 1]; //Adicionou centenas
+
+                    i = nums[1].ToString();
+                    j = int.Parse(i);
+
+                    if (j < 2) //Verificar se é um número inferior a 20
+                    {
+                        string k = nums[1].ToString() + nums[2].ToString();
+                        int l = int.Parse(k);
+                        if (l > 0) //Verificar se há unidades maior que 0
+                        {
+                            extenso = extenso + " e " + unidades[l];
+                        }
+                        return extenso;
+                    }
+                    else //Se for superior a 20
+                    {
+                        extenso = extenso + " e " + dezenas[j - 2];
+                        i = nums[2].ToString();
+                        j = int.Parse(i);
+                        if (j > 0)
+                        {
+                            extenso = extenso + " e " + unidades[j];
+                        }
+                        return extenso;
+                    }
+                }
+                else
+                {
+                    Console.Write("Valor inválido: ");
+                    return val.ToString();
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Valor val1 = new Valor();
+            do
+            {
+                Console.Write("Digite um valor a partir de 0 até 999: ");
+                val1.AtribuiValor = int.Parse(Console.ReadLine());
+                Console.WriteLine(val1.NumExtenso());
+            } while (val1.RetornaValor != 1000); //Caso queira encerrar digite um valor inválido
+        }
+    }
+}
